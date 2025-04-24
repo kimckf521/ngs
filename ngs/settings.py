4eff5377df33781c663272fe7e8232707106a568
Kim <kim@Pauls-Mac-mini.local>
2025-04-24 21:15:41 +1000

    Added domain

--- ngs/settings.py
+++ ngs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g_2&-2_h2v(9z6hdkzntx#5&s9$5lgmfud9gbdr#)-7@w$do&v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextgenscholar.aisa']
 
 
 # Application definition
